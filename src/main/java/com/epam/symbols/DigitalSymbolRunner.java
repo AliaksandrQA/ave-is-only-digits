81a50b45f174bc154c666b92979e5e1412389a42
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-10-18 15:24:53 +0200

    refactoring

--- src/main/java/com/epam/symbols/DigitalSymbolRunner.java
+++ src/main/java/com/epam/symbols/DigitalSymbolRunner.java
@@ -3,11 +3,16 @@ package main.java.com.epam.symbols;
 public class DigitalSymbolRunner {
     public static void main(String[] args) {
         String str = "123456789cat";
-        isDigital(str);
+        boolean isTotallyNumeric = hasOnlyDigits(str);
+        showResult(isTotallyNumeric);
     }
 
-    private static void isDigital(String str) {
+    private static boolean hasOnlyDigits(String str) {
         boolean isNumeric = str.chars().allMatch(x -> Character.isDigit(x));
+        return isNumeric;
+    }
+
+    private static void showResult(boolean isNumeric) {
         System.out.println(isNumeric);
     }
 }
